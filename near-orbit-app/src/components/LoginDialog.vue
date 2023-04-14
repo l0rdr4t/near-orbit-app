d363876711aee1980750648eaf245f5eeafcd386
leeryantech <inbox@leeryan.au>
2023-04-15 00:03:40 +0800

    testing Vue application

---
+++ near-orbit-app/src/components/LoginDialog.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <h2>welcome to nearOrbit.app</h2>
+        <button @click="discordLogin()">Login with Discord</button>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'LoginDialog',
+    methods: {
+        discordLogin() {
+            this.$login(`/redirection/test`, (response) => {
+                console.log(response.data['isAuthenticated']) // true or false
+                this.user = response.data['user']; // save user in session
+                window.location.href = response.data['redirect']; // for redirect to page
+            })
+
+            this.$disconnect(() => {
+                //callback
+
+                window.location.href = "/"; // redirect to /
+            })
+        }
+    }
+};
+
+
+
+</script>
---
+++ near-orbit-app/src/views/LoginView.vue
@@ -0,0 +1,18 @@
+<template>
+    <LoginDialog />
+</template>
+
+<script>
+
+// @ is an alias to /src
+import LoginDialog from '@/components/LoginDialog.vue'
+
+export default {
+    name: 'LoginView',
+    components: {
+        LoginDialog
+    }
+}
+</script>
+
+<style></style>
