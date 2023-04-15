66584548bcc7b442c63621b93b5b412661cc878c
l0rdr4t <33777123+l0rdr4t@users.noreply.github.com>
2023-04-15 20:21:49 +0800

    Develop (#2)
    * update README.md
    
    * updated AboutMain button
    
    * customised @websanova/vue-auth
    
    * added token call
    
    ---------
    
    Co-authored-by: l0rdr4t <l0rdr4t@nearorbit.app>

---
+++ src/components/LoginDiscordMain.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <span
+            v-show="!$auth.ready()"
+        >
+            Loading...
+        </span>
+
+        <span
+            v-show="$auth.ready()"
+        >
+            Ready!
+        </span>
+    </div>
+    <div>
+        <h2>welcome to nearOrbit.app</h2>
+        <button @click="oauth2()">Login with Discord</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'LoginMain',
+    methods: {
+        oauth2() {
+            this.$auth
+                .oauth2('discord', {
+                    url: 'auth/discord',
+                    code: true,
+                    body: {
+                        code: this.$route.query.code
+                    },
+                    state: this.$route.query.state
+                });
+        }
+    }
+}
+</script>
