2e1c27c9610b46127707ef03acde569fd36ea160
l0rdr4t <33777123+l0rdr4t@users.noreply.github.com>
2023-04-16 18:49:18 +0800

    updated route with auth

--- src/components/LoginDiscordMain.vue
+++ src/components/LoginDiscordMain.vue
@@ -4,26 +4,52 @@
     </div>
 </template>
 
-<script setup>
+<script>
 import axios from 'axios';
-import { useRoute } from "vue-router";
-import { onMounted } from 'vue';
-import store from '../store'
 
-const route = useRoute()
-const req = {
-    code: route.query.code,
-    state: route.query.state
+export default {
+    name: 'LoginDiscordMain',
+    data() {
+        return {
+            loading: false,
+            post: null,
+            error: null,
+        }
+    },
+    created() {
+        this.$watch(
+            () => this.$route.params,
+            () => {
+                this.fetchData()
+            },
+            // fetch the data when the view is created and the data is
+            // already being observed
+            { immediate: true }
+        )
+    },
+    methods: {
+        fetchData() {
+            this.error = this.post = null
+            this.loading = true
+            // replace `getPost` with your data fetching util / API wrapper
+            const req = {
+                code: this.$route.query.code,
+                state: this.$route.query.state
+            }
+            // axios.post('https://nearorbit.app/.netlify/functions/handleAuth', req)
+            axios.post('http://localhost:8888/.netlify/functions/handleAuth', req)
+                .then(response => {
+                    const token = response.data;
+                    alert(token);
+                    this.$store.commit('setToken', token);
+                    window.close();
+                })
+                .catch(e => {
+                    const token = e.toString();
+                    alert(token);
+                    window.close();
+                })
+        }
+    }
 }
-
-onMounted(
-    axios.post('https://nearorbit.app/.netlify/functions/handleAuth', req)
-    // axios.post('http://localhost:8888/.netlify/functions/handleAuth', req)
-    .then(response => {
-        store.commit('setToken',response.data);
-        alert(response.data);
-        window.close();
-    })
-)
-
 </script>
