4f4b5a21ffc16989fc89bc5b46819eb7593a1dbc
l0rdr4t <33777123+l0rdr4t@users.noreply.github.com>
2023-04-18 23:04:49 +0800

    updated handler to wait for response

--- functions/handleAuth.js
+++ functions/handleAuth.js
@@ -1,53 +1,42 @@
 // functions/handleAuthCode.js
 
 const { default: axios } = require("axios");
+const oauthSecret = process.env.DISCORD_SECRET;
 
 exports.handler = async function (event, context) {
     if (event.httpMethod !== 'POST') {
         return { statusCode: 405, body: 'Method Not Allowed' };
     }
 
-    // var axios = new axios();
-    const oauthSecret = process.env.DISCORD_SECRET;
     const body = JSON.parse(event.body);
     const code = body.code;
     const state = body.state;
 
-    // Handle the OAuth secret and authorization code here.
-    // For example, exchange the authorization code for an access token.
-    // ...
-    const data = new URLSearchParams(Object.entries({
+    const data = {
         grant_type: 'authorization_code',
         client_id: '1096086421221150812',
         client_secret: oauthSecret,
-        redirect_uri: 'https://nearorbit.app/login/discord',
+        redirect_uri: 'http://localhost:8888/login/discord',
         code: code,
         state: state
-    }));
+    };
 
-    const res = await axios.post('https://discord.com/api/oauth2/token', data.toString(), {
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-        }
-    });
+    let response = {};
 
-    const resStr = JSON.stringify(res);
-
-    if (res.status == '200') {
-        return {
-            statusCode: 200,
-            body: JSON.stringify({
-                message: 'OAuth secret and authorization code handled',
-                res: resStr
-            }),
-        };
-    } else {
-        return {
-            statusCode: res.status,
-            body: JSON.stringify({
-                message: res.statusText,
-                res: resStr
-            }),
-        };
+    try {
+        const res = await axios.post('https://discord.com/api/oauth2/token', data, {
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        });
+        response = res;
+    } catch (err) {
+        response = err.response;
+        response.status = response.status || 500;
     }
+
+    return {
+        statusCode: response.status,
+        body: JSON.stringify(response.data)
+    };
 };
--- src/components/LoginDiscordMain.vue
+++ src/components/LoginDiscordMain.vue
@@ -32,27 +32,20 @@ export default {
         fetchData() {
             this.error = this.post = null
             this.loading = true
-            // replace `getPost` with your data fetching util / API wrapper
             const req = {
                 code: this.$route.query.code,
                 state: this.$route.query.state
             }
-
-            alert("authTarget: " + this.authTarget);
-            alert("$route.query.code: " + req.code);
-            alert("$route.query.state: " + req.state);
-            // axios.post('', req)
             axios.post(this.authTarget, req)
                 .then(response => {
                     const token = response.data;
-                    alert("success: " + token);
                     this.$store.commit('setToken', token);
                 })
                 .catch(e => {
                     const token = e.toString();
                     alert("error: " + token);
                 });
-            // window.close();
+            window.close();
         }
     }
 }
